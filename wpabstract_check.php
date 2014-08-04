3837d08b2423e8f4c29eaa54652232758598595d
root <root@basheersubei.com>
2014-08-04 04:15:40 -0400

    fixed up the abstract form code

--- wpabstract_check.php
+++ wpabstract_check.php
@@ -1,15 +1,9 @@
-// place these in line 27 of page.php
-			// displays abstract submission form if user has "abstract" role
-                            <?php if(is_page( 'Abstract Submission' )) :
-                                    if ( current_user_can('abstract') ) :
-                                            echo do_shortcode("[wpabstracts]");
-                                    endif;
-                            endif; ?>
-
-
 // place these at the end of page.php
    //  hides abstract submission link for non-abstract users and hides event registration link for already registered users
    <?php if ( current_user_can('abstract') ) {
+ 			    if(is_page( 'Abstract Submission' )) :
+                                        echo do_shortcode("[wpabstracts]");
+                                endif;
                             echo '<style type="text/css">
                                 #menu-item-38 {
                                     display: none;
