8b26f511f8980c63a2b14cf41b6abdb33ada626c
root <root@basheersubei.com>
2014-08-04 04:09:40 -0400

    now hides abstract and event links depending on who's logged in

--- wpabstract_check.php
+++ wpabstract_check.php
@@ -7,3 +7,19 @@
                             endif; ?>
 
 
+// place these at the end of page.php
+   //  hides abstract submission link for non-abstract users and hides event registration link for already registered users
+   <?php if ( current_user_can('abstract') ) {
+                            echo '<style type="text/css">
+                                #menu-item-38 {
+                                    display: none;
+                                }
+                                </style>';
+                        } else {
+                            echo '<style type="text/css">
+                                #menu-item-39 {
+                                    display: none;
+                                }
+                                </style>';
+ 
+                        } ?>
